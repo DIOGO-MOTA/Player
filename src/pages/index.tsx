568a815b01ac850b780881f362845731a9e3a9a8
DIOGO-MOTA <diogomota21@gmail.com>
2021-07-02 17:35:14 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,28 @@
+import { GetStaticProps } from 'next'
 
-export default function Home() {
+type Episode = {
+  id: string;
+  title: string;
+  menbers: string;
+}
+type HomeProps = {
+  episodes: Array<Episode>;
+}
+
+export default function Home(props: HomeProps) {
   return (
-  <> </>
+    <> </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch('http://localhost:3333/episodes?_limit=12&_sort=publiched_at&order=desc')
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  }
 }
